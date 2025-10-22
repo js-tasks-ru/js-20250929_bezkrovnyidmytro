7e697d1aba830324041a6aed5666cdcdc3b8ca35
bezkrovnyidmytro <bezkrovnyidmytro@gmail.com>
2025-10-22 15:42:22 +0200

    Try to add SortableTable V2

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,92 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import defaultExport from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
+export default class SortableTable extends defaultExport {
+  sorted = null;
+  sortableHeaderItems = null;
+  isSortLocally = true;
+
+  constructor(headersConfig, { data = [], sorted = {} } = {}) {
+    super(headersConfig, data);
+    this.element = super.getElement();
+    this.sorted = sorted;
+    this.sortableHeaderItems = this.getSortableHeaderElements();
+
+    this.addSortArrowToDefault(sorted.id);
+    this.sort(sorted.id, sorted.order);
+
+    this.addEventListeners();
+  }
+
+  addSortArrowToDefault(id) {
+    this.element
+      .querySelector(`[data-element="header"] [data-id="${id}"]`)
+      ?.appendChild(this.arrowElement);
+  }
+
+  onHeaderClick = (event) => {
+    const parent = event.target.parentNode;
+
+    parent.appendChild(this.arrowElement);
+
+    const sortBy = parent.dataset.id;
+    const sortOrder = parent.dataset.sortOrder;
+
+    if (!sortOrder) {
+      parent.dataset.sortOrder = "asc";
+    } else {
+      parent.dataset.sortOrder =
+        parent.dataset.sortOrder === "asc" ? "desc" : "asc";
+    }
+
+    this.sort(sortBy, sortOrder);
+  };
+
+  getSortableHeaderElements() {
+    const sortableHeaderItems = Array.from(
+      this.subElements.header.querySelectorAll(
+        '[data-element="header"] [data-sortable="true"]'
+      )
+    );
+
+    return sortableHeaderItems && sortableHeaderItems.length
+      ? sortableHeaderItems
+      : [];
+  }
+
+  sort(sortBy, sortOrder) {
+    if (this.isSortLocally) {
+      this.sortOnClient(sortBy, sortOrder);
+    } else {
+      this.sortOnServer(sortBy, sortOrder);
+    }
+  }
+
+  sortOnClient(sortBy, sortOrder) {
+    super.sort(sortBy, sortOrder);
+  }
+
+  sortOnServer(sortBy, sortOrder) {
+    super.sort(sortBy, sortOrder);
+  }
+
+  addEventListeners() {
+    this.sortableHeaderItems.forEach((element) => {
+      element.addEventListener("pointerdown", this.onHeaderClick);
+    });
+  }
+
+  removeEventListeners() {
+    this.sortableHeaderItems.forEach((element) => {
+      element.removeEventListener("pointerdown", this.onHeaderClick);
+    });
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.removeEventListeners();
+    this.remove();
   }
 }
